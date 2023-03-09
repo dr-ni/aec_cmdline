49d3e802a12f859345e9eb8ac2cf5f2e2548c68d
dr-ni <68241100+dr-ni@users.noreply.github.com>
2023-03-09 17:39:46 +0100

    Update aec_cmdline.c

--- src/aec_cmdline.c
+++ src/aec_cmdline.c
@@ -1,6 +1,6 @@
 /*
  * aec_cmdline.c
- * A straight forward AEC proove of concept based on speexdsp
+ * A straight forward AEC commandline tool based on speexdsp
  * MIT License Copyright (c) 2022 U. Niethammer
  * compile: gcc aec_cmdline.c -lspeexdsp -o aec_cmdline
  * performs best with newest https://github.com/xiph/speexdsp.git
